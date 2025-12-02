73cf4f74d2c11333b77627e15292d931e32373e2
Jon Cram <webignition@gmail.com>
2025-12-02 17:17:50 +0000

    Use php cs fixer ^3.9

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,4 +14,8 @@ return $config->setRules([
     'trailing_comma_in_multiline' => false,
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
+    'single_line_empty_body' => false,
+    'operator_linebreak' => false,
+    'fully_qualified_strict_types' => false,
+    'string_implicit_backslashes' => false,
 ])->setFinder($finder);
